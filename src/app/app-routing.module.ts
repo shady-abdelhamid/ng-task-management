d4d941b343a3f981114cc7a929713df698e28ea5
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-11-15 16:55:36 +0200

    Add lazy loaded Tasks module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) 
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-task-management';
+  title = 'Task Management';
 }
